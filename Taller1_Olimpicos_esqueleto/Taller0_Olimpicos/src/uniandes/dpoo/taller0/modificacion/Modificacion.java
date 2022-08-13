8b44e4399dd3b344d2107baf8867c5c0ef3157e5
Valeria Torres <98570634+ValeriaTorresG@users.noreply.github.com>
2022-08-13 16:37:32 -0500

    Add files via upload

---
+++ Taller1_Olimpicos_esqueleto/Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,41 @@
+package uniandes.dpoo.taller0.modificacion;
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Modificacion {
+	
+	public void main() throws FileNotFoundException, IOException
+	{
+		System.out.println("Hola, mundo!");
+		try
+		{
+			CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+			
+			//System.out.println(calc.paisConMasMedallistas());
+			
+			Map<String, Integer> pais = calc.paisConMasMedallistas();
+			Set<String> mapNombre = pais.keySet();
+			List<String> keys = new ArrayList<>(mapNombre);
+			String key = keys.toString();
+			String nombre = key.substring(1, key.length()-1);
+			int medallas = pais.get(nombre);
+			System.out.println("El pais con mas medallas es " + nombre + " con " + medallas + " medallas");
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println("No fue posible leer el archivo especificado");
+		}
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException
+	{
+		Modificacion consola = new Modificacion();
+		consola.main();
+	}
+}
